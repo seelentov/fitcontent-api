453ce6c5098480379570f2931336fcc8ccf7e59b
vladisluw <komkov222111@gmail.com>
2024-11-15 11:13:36 +0500

    Update User.php

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable implements JWTSubject, FilamentUser
+
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -65,8 +68,10 @@ class User extends Authenticatable implements JWTSubject
         ];
     }
 
-
-    // Rest omitted for brevity
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->email === env("ADMIN_EMAIL");
+    }
 
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
