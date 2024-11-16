222f303061484884dabc23926887f996ce55410f
vladisluw <komkov222111@gmail.com>
2024-11-17 04:09:21 +0500

    Create ExampleTest.php

---
+++ laravel/tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\BaseServices;
+
+use Illuminate\Support\Facades\Redis;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+
+    public function it_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
