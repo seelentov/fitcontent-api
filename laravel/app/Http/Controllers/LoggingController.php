5663f65380350235495e97e03445c2a4eb5b034f
vladisluw <komkov222111@gmail.com>
2024-11-17 00:01:27 +0500

    Update LoggingController.php

--- laravel/app/Http/Controllers/LoggingController.php
+++ laravel/app/Http/Controllers/LoggingController.php
@@ -14,7 +14,7 @@ class LoggingController extends Controller
             'password' => 'required',
         ]);
 
-        if (Auth::attempt($credentials) && Auth::user()->email === env('ADMIN_EMAIL')) {
+        if (auth()->attempt($credentials) && auth()->user()->email === env('ADMIN_EMAIL')) {
 
             $request->session()->regenerate();
 
@@ -33,6 +33,6 @@ class LoggingController extends Controller
 
     public function me()
     {
-        return response()->json(Auth::user());
+        return response()->json(auth()->user());
     }
 }
