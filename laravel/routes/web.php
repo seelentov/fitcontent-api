c03cb2ce603e29fa09983edb48d0eb161c18fd03
vladisluw <komkov222111@gmail.com>
2024-11-16 23:39:44 +0500

    Update web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\LoggingController;
 use Illuminate\Support\Facades\Route;
 use Filament\Facades\Filament;
 
-Route::group(["prefix" => "logging", 'middleware' => 'web'], function () {
+Route::group(["prefix" => "logging", 'middleware' => 'api'], function () {
     Route::post('auth', [LoggingController::class, 'auth'])->name('logging_auth');
     Route::get('auth', [LoggingController::class, 'login'])->name('logging_login');
 });
