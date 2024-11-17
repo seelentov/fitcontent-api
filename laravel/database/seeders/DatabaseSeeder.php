48978ffac1eea59f2659d69460b5b6d013432f93
vladisluw <komkov222111@gmail.com>
2024-11-17 05:37:27 +0500

    Update DatabaseSeeder.php

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             FolderSeeder::class,
             FileSeeder::class,
+            InfoSeeder::class,
         ]);
     }
 }
