a7a1b754dfe9219117f8d6b9c456299618002e00
vladisluw <komkov222111@gmail.com>
2024-11-17 05:35:10 +0500

    add Info table

---
+++ laravel/app/Filament/Resources/InfoResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\InfoResource\Pages;
+use App\Filament\Resources\InfoResource\RelationManagers;
+use App\Models\Info;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class InfoResource extends Resource
+{
+    protected static ?string $model = Info::class;
+
+    protected static ?string $recordTitleAttribute = 'header';
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('header')
+                    ->required(),
+                Forms\Components\RichEditor::make('text')
+                    ->required(),
+                Forms\Components\TextInput::make('position')
+                    ->numeric()
+                    ->minValue(0)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('header')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('text'),
+                Tables\Columns\TextColumn::make('position')
+                    ->sortable(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['header', 'text'];
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListInfos::route('/'),
+            'create' => Pages\CreateInfo::route('/create'),
+            'edit' => Pages\EditInfo::route('/{record}/edit'),
+        ];
+    }
+}
