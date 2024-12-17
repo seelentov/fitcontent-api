a82e1831261e5e64afc687a3934896de4ade45f3
vladisluw <komkov222111@gmail.com>
2024-12-18 02:33:26 +0500

    Update TestController.php

--- laravel/app/Http/Controllers/TestController.php
+++ laravel/app/Http/Controllers/TestController.php
@@ -14,35 +14,18 @@ class TestController extends Controller
     }
     public function test()
     {
-        try {
-            $res = $this->client->getClient()->request("GET");
-            $res->getStatusCode();
+        $res = $this->client->getClient()->request("GET");
+        $res->getStatusCode();
 
-            if ($res->getStatusCode() >= 400) {
-                $errorContent = $res->getBody()->getContents();
-                \Log::error("Request failed with status code: " . $res->getStatusCode() . " and content: " . $errorContent);
-
-                throw new \Exception("Request failed with status code: " . $res->getStatusCode() . " and content: " . $errorContent);
-            }
-
-
-            $xmlString = $res->getBody()->getContents();
-            $posRes = $this->client->xmlToJson($xmlString);
-            return $posRes;
-        } catch (\GuzzleHttp\Exception\ClientException $e) {
-            $response = $e->getResponse();
-            $errorContent = $response->getBody()->getContents();
-            \Log::error("Client error: " . $e->getMessage() . "  Response: " . $errorContent);
-            return ['error' => $errorContent, 'status_code' => $response->getStatusCode()];
-        } catch (\GuzzleHttp\Exception\ServerException $e) {
-            $response = $e->getResponse();
-            $errorContent = $response->getBody()->getContents();
-            \Log::error("Server error: " . $e->getMessage() . " Response: " . $errorContent);
-            return ['error' => $errorContent, 'status_code' => $response->getStatusCode()];
-        } catch (\Exception $ex) {
-            \Log::error("General error: " . $ex->getMessage());
-            return ['error' => $ex->getMessage()];
+        if ($res->getStatusCode() !== 200) {
+            $errorString = $res->getBody()->getContents();
+            return $errorString;
         }
+
+        $xmlString = $res->getBody()->getContents();
+        $posRes = $this->client->xmlToJson($xmlString);
+
+        return $posRes;
     }
 
 }
