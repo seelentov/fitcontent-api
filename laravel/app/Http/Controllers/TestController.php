471046e79b5c4a009342cc129f27c8e7d67629e2
vladisluw <komkov222111@gmail.com>
2024-12-18 02:28:25 +0500

    Update TestController.php

--- laravel/app/Http/Controllers/TestController.php
+++ laravel/app/Http/Controllers/TestController.php
@@ -16,11 +16,33 @@ class TestController extends Controller
     {
         try {
             $res = $this->client->getClient()->request("GET");
-            $xmlString = $res->getBody();
+            $res->getStatusCode();
+
+            if ($res->getStatusCode() >= 400) {
+                $errorContent = $res->getBody()->getContents();
+                \Log::error("Request failed with status code: " . $res->getStatusCode() . " and content: " . $errorContent);
+
+                throw new \Exception("Request failed with status code: " . $res->getStatusCode() . " and content: " . $errorContent);
+            }
+
+
+            $xmlString = $res->getBody()->getContents();
             $posRes = $this->client->xmlToJson($xmlString);
             return $posRes;
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+            $errorContent = $response->getBody()->getContents();
+            \Log::error("Client error: " . $e->getMessage() . "  Response: " . $errorContent);
+            return ['error' => $errorContent, 'status_code' => $response->getStatusCode()];
+        } catch (\GuzzleHttp\Exception\ServerException $e) {
+            $response = $e->getResponse();
+            $errorContent = $response->getBody()->getContents();
+            \Log::error("Server error: " . $e->getMessage() . " Response: " . $errorContent);
+            return ['error' => $errorContent, 'status_code' => $response->getStatusCode()];
         } catch (\Exception $ex) {
-            return $ex;
+            \Log::error("General error: " . $ex->getMessage());
+            return ['error' => $ex->getMessage()];
         }
     }
+
 }
