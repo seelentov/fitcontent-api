e5277ea2e4fb6ae08441a49d2c40670f387d3afc
vladisluw <komkov222111@gmail.com>
2024-12-18 08:22:38 +0500

    add test2

--- laravel/app/Http/Controllers/TestController.php
+++ laravel/app/Http/Controllers/TestController.php
@@ -7,6 +7,7 @@ use App\Services\FileService\IFileService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Aws\S3\S3Client;
+use Illuminate\Support\Facades\Crypt;
 
 class TestController extends Controller
 {
@@ -16,81 +17,13 @@ class TestController extends Controller
     }
     public function test()
     {
-        // $aws_access_key_id = env('AWS_ACCESS_KEY_ID');
-        // $aws_secret_access_key = env('AWS_SECRET_ACCESS_KEY');
-
-        // $bucket_name = 'fitcontent';
-        // $mainlink = 'https://storage.yandexcloud.net/fitcontent/';
-
-        // $s3 = new S3Client([
-        //     'version' => 'latest',
-        //     'region' => 'ru-central1', // You might need to adjust this region
-        //     'credentials' => [
-        //         'key' => $aws_access_key_id,
-        //         'secret' => $aws_secret_access_key,
-        //     ],
-        //     'endpoint' => 'https://storage.yandexcloud.net',
-        // ]);
-
-        // $files = $this->listFiles($s3, $bucket_name);
-        // $programs = $this->classifyFiles($files);
-        // $jsonData = $this->generateJson($programs, $mainlink);
-
-        // return $jsonData;
-
         return $this->fileService->getObjects();
     }
 
-    private function listFiles(S3Client $s3, string $bucketName): array
+    public function test2()
     {
-        $fileList = [];
-        $paginator = $s3->getPaginator('ListObjectsV2', [
-            'Bucket' => $bucketName,
-        ]);
-
-        foreach ($paginator as $page) {
-            foreach ($page['Contents'] as $object) {
-
-                $fileList[]['size'] = $object['Size'];
-                $fileList[]['created_at'] = $object['LastModified'];
-            }
-        }
-        return $fileList;
-    }
-
-    private function classifyFiles(array $fileList): array
-    {
-        $programs = [];
-        foreach ($fileList as $filePath) {
-            $parts = explode('/', $filePath);
-            if (count($parts) > 2) {
-                $programName = $parts[1];
-                if (!isset($programs[$programName])) {
-                    $programs[$programName] = [
-                        'pdfUrl' => '',
-                        'videoUrl' => '',
-                        'musicUrls' => [],
-                        'subtitleUrl' => '',
-                    ];
-                }
-                $extension = pathinfo($filePath, PATHINFO_EXTENSION);
-                if ($extension === 'pdf') {
-                    $programs[$programName]['pdfUrl'] = $filePath;
-                } elseif (in_array($extension, ['mp4', 'avi', 'mov'])) {
-                    $programs[$programName]['videoUrl'] = $filePath;
-                } elseif ($extension === 'mp3') {
-                    $programs[$programName]['musicUrls'][] = $filePath;
-                }
-            }
-        }
-        return $programs;
-    }
-
-    private function generateJson(array $programs, string $baseUrl): string
-    {
-        return json_encode([
-            'baseUrl' => $baseUrl,
-            'programs' => $programs,
-        ]);
+        dump(Crypt::decryptString("eyJpdiI6InBKaFNGRm1ZNlZCMFBYY0phR1p1MlE9PSIsInZhbHVlIjoidng4ZDlma1Rjb1VhdXRFbzlrS2IwQT09IiwibWFjIjoiZDJkZjg1NzYyZjNiOTU0NTA0ZGUxNjBiYjcyN2UyN2ViMDhkMjdlYzBiMzY2ZmJjODhlYzU2ZDVkOWJjOGFiZiIsInRhZyI6IiJ9"));
+        dd(Crypt::decryptString("eyJpdiI6InNYZ1RSYlUwZVh6NlgrWjR6d1BMdFE9PSIsInZhbHVlIjoiZ0RTdThXbkU2QnBCNFErcFY5bnA3QT09IiwibWFjIjoiNTYyODg4MGE0MjAxNmFmYmJlYTExNjljMDc0YmU0M2VkZWZkNTdjNjFkOGUwY2JhY2VjZjIzMjdlNWRjOGE2MSIsInRhZyI6IiJ9"));
+        return;
     }
 }
