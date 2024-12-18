b2d441ce809dca1d969f02ae7322ad458022d05f
seelentov <komkov222111@gmail.com>
2024-12-18 23:22:06 +0500

    upd FileService

--- laravel/app/Services/FileService/FileService.php
+++ laravel/app/Services/FileService/FileService.php
@@ -112,14 +112,14 @@ class FileService extends Service implements IFileService
 
     private function getObjects()
     {
-        // if (Redis::exists($this->redisKey)) {
-        // return json_decode(Redis::get($this->redisKey));
-        // }
+        if (Redis::exists($this->redisKey)) {
+            return json_decode(Redis::get($this->redisKey), true);
+        }
 
         $data = $this->getObjectsCore();
 
-        // Redis::set($this->redisKey, json_encode($data));
-        // Redis::expire($this->redisKey, $this->redisTTL);
+        Redis::set($this->redisKey, json_encode($data));
+        Redis::expire($this->redisKey, $this->redisTTL);
 
         return $data;
     }
